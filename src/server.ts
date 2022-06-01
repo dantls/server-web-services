e8b9b849816c80e46241b3059b2bfc1410276779
dantls <dan_tls@hotmail.com>
2022-06-01 11:05:46 -0400

    Change Port

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@
 import http from './app';
 import pool from './database/pgconnector';
 
-http.listen(3333, () => {
-  console.log("Server is running on port 3333");
+http.listen(3335, () => {
+  console.log("Server is running on port 3335");
   pool.connect(function (err, client, done) {
     if (err) 
       throw new Error();
